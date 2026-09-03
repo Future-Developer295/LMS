a765b53f4c276e484a628a560feee257b226943c
Gen-z code <ak3952009@gmail.com>
2026-09-03 23:11:02 +0500

    Bug Fix

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,15 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ClassController;
 use App\Http\Controllers\StudentClassController;
 use App\Http\Controllers\StudentAuthController;
 use App\Http\Controllers\StudentRegisterController;
 use App\Http\Controllers\FrontendController;
-use App\Http\Controllers\AttendanceController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ClassController;
+use Illuminate\Support\Facades\Auth;
 
 Route::middleware('auth')->group(function () {
-
-
 
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
@@ -24,58 +22,18 @@ Route::middleware('auth')->group(function () {
     Route::get('/dashboard/student/edit', [DashboardController::class, 'student_edit'])->name('student_edit');
     Route::get('/dashboard/student/add', [DashboardController::class, 'student_add'])->name('student_add');
 
-    Route::get('/dashboard/class', [DashboardController::class, 'class'])->name('class');
-    Route::get('/dashboard/class/edit', [DashboardController::class, 'class_edit'])->name('class_edit');
-    Route::get('/dashboard/class/add', [DashboardController::class, 'class_add'])->name('class_add');
+    Route::get('/dashboard/class', [ClassController::class, 'class'])->name('class');
+Route::get('/dashboard/class/add', [ClassController::class, 'class_add'])->name('class_add');
+Route::post('/dashboard/class/store', [ClassController::class, 'class_store'])->name('class_store');
+Route::get('/dashboard/class/edit/{id}', [ClassController::class, 'class_edit'])->name('class_edit');
+Route::put('/dashboard/class/update/{id}', [ClassController::class, 'class_update'])->name('class_update');
+Route::delete('/dashboard/class/delete/{id}', [ClassController::class, 'destroy'])
+    ->name('class_destroy');
+Route::get('/class/view/{id}', [ClassController::class, 'view'])->name('class_view');
 
-
-
-
-
-
-
-
-    //attendeance 
-
-
-
- Route::get('/dashboard/attendance', [AttendanceController::class, 'index'])
-    ->name('attendance');
-
-Route::get('/dashboard/attendance/add', [AttendanceController::class, 'create'])
-    ->name('attendance_add');
-
-Route::get('/dashboard/attendance/view/{attendance}', [AttendanceController::class, 'show'])
-    ->name('attendance_view');
-
-Route::get('/dashboard/attendance/edit/{attendance}', [AttendanceController::class, 'edit'])
-    ->name('attendance_edit');
-
-Route::get('/dashboard/attendance/students/{batch_code}', [AttendanceController::class, 'studentsByBatch'])
-    ->name('attendance_students');
-
-Route::post('/dashboard/attendance/store', [AttendanceController::class, 'store'])
-    ->name('attendance_store');
-
-Route::put('/dashboard/attendance/update/{attendance}', [AttendanceController::class, 'update'])
-    ->name('attendance_update');
-
-Route::delete('/dashboard/attendance/delete/{attendance}', [AttendanceController::class, 'destroy'])
-    ->name('attendance_destroy');
-
-
-
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-
-    Route::get('/dashboard/teacher', [DashboardController::class, 'teacher'])->name('teacher');
-    Route::get('/dashboard/teacher/edit', [DashboardController::class, 'teacher_edit'])->name('teacher_edit');
-    Route::get('/dashboard/teacher/add', [DashboardController::class, 'teacher_add'])->name('teacher_add');
-
-
-
-
-
-
+    Route::get('/dashboard/attendance', [DashboardController::class, 'attendance'])->name('attendance');
+    Route::get('/dashboard/attendance/edit', [DashboardController::class, 'attendance_edit'])->name('attendance_edit');
+    Route::get('/dashboard/attendance/add', [DashboardController::class, 'attendance_add'])->name('attendance_add');
 
     Route::get('/dashboard/assignment', [DashboardController::class, 'assignment'])->name('assignment');
     Route::get('/dashboard/assignment/edit', [DashboardController::class, 'assignment_edit'])->name('assignment_edit');
@@ -85,8 +43,6 @@ Route::delete('/dashboard/attendance/delete/{attendance}', [AttendanceController
     Route::get('/dashboard/submission/grade', [DashboardController::class, 'submission_grade'])->name('submission_grade');
 });
 
-Route::middleware('guest')->group(function () {
-
 Route::get('/', [FrontendController::class, 'index'])->name('index');
 Route::get('/class', [FrontendController::class, 'class'])->name('class');
 Route::get('/calendar', [FrontendController::class, 'calendar'])->name('calendar');
@@ -95,6 +51,18 @@ Route::get('/classwork/detail', [FrontendController::class, 'detail'])->name('de
 Route::get('/archived', [FrontendController::class, 'archived'])->name('archived');
 Route::get('/steam', [FrontendController::class, 'steam'])->name('steam');
 Route::get('/people', [FrontendController::class, 'people'])->name('people');
+
+Route::middleware('auth')->group(function () {
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware('guest')->group(function () {
+
+    Route::get('/student/login', [StudentAuthController::class, 'showLogin'])
+        ->name('student.login');
 
     Route::post('/student/login', [StudentAuthController::class, 'login'])
         ->name('student.login.submit');
@@ -110,7 +78,8 @@ Route::post('/student/logout', [StudentAuthController::class, 'logout'])
 
 Route::get('/student/profile', function () {
 
-    $user = auth()->user();
+   $user = Auth::user();
+
 
     $student = \App\Models\Student::where(
         'email_address',
@@ -123,7 +92,8 @@ Route::get('/student/profile', function () {
 
 Route::get('/student/profile/complete', function () {
 
-    $user = auth()->user();
+$user = Auth::user();
+
 
     $student = \App\Models\Student::where(
         'email_address',
@@ -136,7 +106,8 @@ Route::get('/student/profile/complete', function () {
 
 Route::post('/student/profile/complete', function (Illuminate\Http\Request $request) {
 
-    $user = auth()->user();
+    $user = Auth::user();
+
 
     $student = \App\Models\Student::where(
         'email_address',
@@ -183,5 +154,4 @@ Route::get('/student/dashboard', function () {
     return view('frontend_theme.index');
 })->middleware('auth')->name('student.dashboard');
 
-require __DIR__ . '/auth.php';
 require __DIR__ . '/auth.php';
